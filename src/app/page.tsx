4671aa1c62089fb8cf137af5546b9ee8ec1f023d
AtulMinz <atulminz.jsr@gmail.com>
2024-03-30 15:29:50 +0530

    Added toggle theme button on top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ModeToggle } from "@/components/theme-switcher";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
@@ -7,7 +8,10 @@ export default function Home() {
   const router = useRouter();
   return (
     <main>
-      <div className="min-h-screen flex justify-center items-center flex-col gap-10">
+      <span className="flex justify-end m-3">
+        <ModeToggle />
+      </span>
+      <div className="flex justify-center items-center flex-col gap-10 p-[200px]">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-gray-300">
           Welcome to Flowfund
         </h1>
